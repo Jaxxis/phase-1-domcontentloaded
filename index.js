c97553efcac15fb24b24022236ad04173a081344
Jake Mckenzie-Edwards <jake.mckenzieedwards@gmail.com>
2023-04-19 14:32:20 +1000

    commit tree 4d40ec29e1253200ecac96a0fb8356b2f65f039c

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Your code goes here
+document.addEventListener('DOMContentLoaded', function(){
+    const editText = document.getElementById('text')
+    editText.textContent = 'This is really cool!'
+})
